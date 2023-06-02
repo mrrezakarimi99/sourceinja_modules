555fdcaff2092cb83582731abaed42d0a2a56d10
reza <mr.rezakarimi99@gmail.com>
2023-06-03 00:21:38 +0330

    add base package

---
+++ src/Console/Commands/SourceinjaListModule.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Sourceinja\RegisterModule\Console\Commands;
+
+use Exception;
+use Illuminate\Console\Command;
+use Illuminate\Support\Str;
+use Sourceinja\RegisterModule\RegisterModule;
+
+class SourceinjaListModule extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'sourceinja:module-list';
+
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'List all modules';
+
+    /**
+     * @throws Exception
+     */
+    public function handle()
+    {
+        $registered = new RegisterModule();
+        $group = $registered->getAllGroups();
+        $sub = $registered->getSubGroups($group['id']);
+        $projects = $registered->getProjects($sub['id']);
+        $this->table(['ID' , 'Name' , 'updated_at'] , $projects);
+    }
+}
